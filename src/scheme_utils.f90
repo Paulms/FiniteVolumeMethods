fdfbbaef15ffc31e58bd297139ea386ef9720843
Paul Mendez <pmendez@ing-mat.udec.cl>
2017-06-19 15:45:01 -0400

    Add cdt for not diffusive problems

--- src/scheme_utils.f90
+++ src/scheme_utils.f90
@@ -42,6 +42,40 @@ function cdtdiff(u, prob, CFL) result(dt)
   DEALLOCATE(B, J)
 end function cdtdiff
 
+function cdt(u, prob, CFL) result(dt)
+  type(CLS1DDiffusionProblem) :: prob
+  REAL(kind = dp), intent(in)  :: u(:,:), CFL
+  REAL(kind = dp)              :: dt, dx
+  REAL(kind = dp)              :: maxp
+  character jobvl, jobvr
+  real (kind = dp)              :: WR(size(u,2)), WL(size(u,2)), work(1000)
+  real (kind = dp)              :: VL(size(u,2),size(u,2)), VR(size(u,2),size(u,2))
+  REAL(kind = dp), ALLOCATABLE  :: J(:,:)
+  INTEGER :: M, N, info, ldvl, ldvr, lwork, i
+  ! =========================
+  dx = prob%mesh%dx
+  maxp = 0
+  N = size(u,1)
+  M = size(u,2)
+  jobvl = 'N'
+  jobvr = 'N'
+  LDVL = M; LDVR = M; lwork = -1
+  WR = 0.0_dp; WL = 0.0_dp; VL = 0.0_dp; VR = 0.0_dp
+  ALLOCATE(J(M,M))
+  J = 0.0_dp
+  do i = 1,N
+    J = prob%Jf(u(i,:))
+    call dgeev (jobvl, jobvr, M, J, M, WR, WL, VL, LDVL, VR, LDVR, WORK, 4*M, INFO)
+    maxp = max(maxp, maxval(abs(WR)))
+    IF (info /= 0) THEN
+      PRINT *, "Error while computing eigenvalues"
+      STOP
+    END IF
+  end do
+  dt = CFL*dx/maxp
+  DEALLOCATE(J)
+end function cdt
+
 function  fluxp(u, JacF) result(pp)
   REAL(kind = dp), intent(in)  :: u(:)
   REAL(kind = dp) :: pp
